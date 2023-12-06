efb98e13406dc803950b87643e5fd5b4eb8583c5
Folka2134 <folka2134@gmail.com>
2023-12-06 12:26:42 +0000

    implemented delete note

--- src/App.tsx
+++ src/App.tsx
@@ -50,6 +50,12 @@ function App() {
     });
   };
 
+  const onDeleteNote = (id: string) => {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  };
+
   const addTag = (tag: Tag) => {
     setTags((prev) => [...prev, tag]);
   };
@@ -72,7 +78,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
