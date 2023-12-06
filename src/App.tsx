672a0828c86b3d6e1577ad0be2fdeb6c1c841a78
Folka2134 <folka2134@gmail.com>
2023-12-06 09:31:16 +0000

    implememented routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import NewNote from "./components/NewNote";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Yo</h1>
-      </div>
-    </>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Hello world</h1>}></Route>
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
   );
 }
 
