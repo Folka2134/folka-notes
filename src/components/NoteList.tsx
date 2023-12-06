607749f5d1d95622a5239aa4778aa223903fd098
Folka2134 <folka2134@gmail.com>
2023-12-06 11:44:24 +0000

    created note card

--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -11,38 +11,6 @@ type NoteListProps = {
   notes: SimpleNote[];
 };
 
-const NoteCard = ({ id, title, tags }: SimpleNote) => {
-  return (
-    <Card
-      as={Link}
-      to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
-    >
-      <Card.Body>
-        <Stack
-          gap={2}
-          className="align-items-center justify-content-center h-100"
-        >
-          <span>{title}</span>
-          {tags.length > 0 && (
-            <Stack
-              gap={1}
-              direction="horizontal"
-              className="justify-content-center flex-wrap"
-            >
-              {tags.map((tag) => (
-                <Badge key={tag.id} className="text-truncate">
-                  {tag.label}
-                </Badge>
-              ))}
-            </Stack>
-          )}
-        </Stack>
-      </Card.Body>
-    </Card>
-  );
-};
-
 const NoteList = ({ availableTags, notes }: NoteListProps) => {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
@@ -118,6 +86,38 @@ const NoteList = ({ availableTags, notes }: NoteListProps) => {
         ))}
       </Row>
     </>
+  );
+};
+
+const NoteCard = ({ id, title, tags }: SimpleNote) => {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span>{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
   );
 };
 
