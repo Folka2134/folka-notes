568432678dc7cb14d83bc4c9997bcdf93184fa80
folka2134 <folka2134@gmail.com>
2024-02-25 09:07:01 +0000

    abstracted EditTagsModal and NoteCard

---
+++ src/components/NoteCard.tsx
@@ -0,0 +1,39 @@
+import { Badge, Card, Stack } from "react-bootstrap";
+import { SimpleNote } from "../types";
+import { Link } from "react-router-dom";
+
+import styles from "../css/NoteList.module.css";
+
+const NoteCard = ({ id, title, tags }: SimpleNote) => {
+  return (
+    <Card
+      as={Link}
+      to={`/${id}`}
+      className={`h-100 text-reset text-decoration-none ${styles.card}`}
+    >
+      <Card.Body>
+        <Stack
+          gap={2}
+          className="align-items-center justify-content-center h-100"
+        >
+          <span>{title}</span>
+          {tags.length > 0 && (
+            <Stack
+              gap={1}
+              direction="horizontal"
+              className="justify-content-center flex-wrap"
+            >
+              {tags.map((tag) => (
+                <Badge key={tag.id} className="text-truncate">
+                  {tag.label}
+                </Badge>
+              ))}
+            </Stack>
+          )}
+        </Stack>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default NoteCard;
--- src/components/NoteList.tsx
+++ src/components/NoteList.tsx
@@ -1,31 +1,14 @@
 import { useMemo, useState } from "react";
-import {
-  Badge,
-  Button,
-  Card,
-  Col,
-  Form,
-  Modal,
-  Row,
-  Stack,
-} from "react-bootstrap";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { SimpleNote, Tag } from "../types";
-
-import styles from "../css/NoteList.module.css";
+import NoteCard from "./NoteCard";
+import EditTagsModal from "./EditTagsModal";
 
 type NoteListProps = {
   availableTags: Tag[];
   notes: SimpleNote[];
-  onDeleteTag: (id: string) => void;
-  onUpdateTag: (id: string, label: string) => void;
-};
-
-type EditTagsModalProps = {
-  availableTags: Tag[];
-  show: boolean;
-  handleClose: () => void;
   onDeleteTag: (id: string) => void;
   onUpdateTag: (id: string, label: string) => void;
 };
@@ -123,79 +106,6 @@ const NoteList = ({
         onUpdateTag={onUpdateTag}
       />
     </>
-  );
-};
-
-const NoteCard = ({ id, title, tags }: SimpleNote) => {
-  return (
-    <Card
-      as={Link}
-      to={`/${id}`}
-      className={`h-100 text-reset text-decoration-none ${styles.card}`}
-    >
-      <Card.Body>
-        <Stack
-          gap={2}
-          className="align-items-center justify-content-center h-100"
-        >
-          <span>{title}</span>
-          {tags.length > 0 && (
-            <Stack
-              gap={1}
-              direction="horizontal"
-              className="justify-content-center flex-wrap"
-            >
-              {tags.map((tag) => (
-                <Badge key={tag.id} className="text-truncate">
-                  {tag.label}
-                </Badge>
-              ))}
-            </Stack>
-          )}
-        </Stack>
-      </Card.Body>
-    </Card>
-  );
-};
-
-const EditTagsModal = ({
-  availableTags,
-  handleClose,
-  show,
-  onUpdateTag,
-  onDeleteTag,
-}: EditTagsModalProps) => {
-  return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Edit Tags</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-          <Stack gap={2}>
-            {availableTags.map((tag) => (
-              <Row key={tag.id}>
-                <Col>
-                  <Form.Control
-                    type="text"
-                    value={tag.label}
-                    onChange={(e) => onUpdateTag(tag.id, e.target.value)}
-                  />
-                </Col>
-                <Col xs="auto">
-                  <Button
-                    onClick={() => onDeleteTag(tag.id)}
-                    variant="outline-danger"
-                  >
-                    &times;
-                  </Button>
-                </Col>
-              </Row>
-            ))}
-          </Stack>
-        </Form>
-      </Modal.Body>
-    </Modal>
   );
 };
 
